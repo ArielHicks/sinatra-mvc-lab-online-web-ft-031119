998c7c030b73e935147b6fcf7e11aba0f17b3fb3
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-26 18:56:30 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+  attr_accessor :word
+
+  # def piglatinize_word(word)
+  #
+  #   first_letter = word[0].downcase
+  #
+  #   if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+  #   else
+  #
+  #
+  # end
+
+
+end
