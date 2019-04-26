b44939f6261cca9d8ad43aeffb5c4d4200e4ea61
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-26 19:59:06 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,5 +15,6 @@ class PigLatinizer
     if !consonant?(word[0])
       word = word + "w"
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
 
 end
