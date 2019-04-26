b53082f619db20d16fecbc79ec429a41584fce29
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-26 19:18:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,7 @@ class PigLatinizer
   attr_accessor :word
 
   def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) :piglatinize_sentence(input_str)
 
 
 end
